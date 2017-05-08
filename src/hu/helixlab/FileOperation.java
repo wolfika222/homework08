c0c4992e487fd0070554f857ce58d01958277b48
Hp_Workplace <farkas0528@gmail.com>
2017-05-08 14:53:16 +0200

    #5

--- src/hu/helixlab/FileOperation.java
+++ src/hu/helixlab/FileOperation.java
@@ -1,6 +1,8 @@
 package hu.helixlab;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 /**
@@ -42,11 +44,35 @@ public class FileOperation {
 
     public static void fileMakerWithContent(String path, int db, String out, String content) {
         try {
-            for (int i = 0; i < db ; i++) {
+            for (int i = 0; i < db; i++) {
                 FileWriter file = new FileWriter(path + i + out);
                 file.write(content);
                 file.close();
             }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void printMatrixFromFile() {
+        int x = 0;
+        int y = 0;
+        int[][] matrix = new int[5][5];
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Hp_Workplace\\IdeaProjects\\homework08\\files\\matrix.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(" ");
+                for (String str : values) {
+                    int str_int = Integer.parseInt(str);
+                    matrix[x][y] = str_int;
+                    System.out.print(matrix[x][y] + " ");
+                }
+                y += 1;
+                System.out.println("");
+            }
+            x += 1;
+
+            br.close();
         } catch (Exception e) {
             System.out.println(e);
         }
--- src/hu/helixlab/Main.java
+++ src/hu/helixlab/Main.java
@@ -8,5 +8,6 @@ public class Main {
         FileOperation.fileMaker("C:\\Users\\Hp_Workplace\\IdeaProjects\\homework08\\files\\", 5, "Test.txt");
         FileOperation.fileMakerWithContent("C:\\Users\\Hp_Workplace\\IdeaProjects\\homework08\\files\\", "output.txt", "Ez a szöveg lesz beleírva a fájlba");
         FileOperation.fileMakerWithContent("C:\\Users\\Hp_Workplace\\IdeaProjects\\homework08\\files\\", 5, "Test.txt", "Ez a szöveg van beleírva a fájlokba");
+        FileOperation.printMatrixFromFile();
     }
 }
